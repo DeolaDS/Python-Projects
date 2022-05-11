4dcdf3e790925a4ad096ff050dc7157b1249b891
DeolaDS <deola.c.dsilva3@gmail.com>
2022-05-11 13:44:14 +0400

    Add app1.py
    Basics

---
+++ app1.py
@@ -0,0 +1,14 @@
+from operator import index
+
+
+'''
+print("Hello World")
+print("Working with\n\"Strings\"")
+sentence = "Hello World"
+print(sentence.index("W"))
+print(sentence[0] + " - "+ sentence[6])
+print(sentence.replace("Hello", "Goodbye"))
+'''
+
+name = input("Enter your name: ")
+print("Hello, " + name)
