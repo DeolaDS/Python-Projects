cac7213c92877c08b78452b69261865e45170ef4
DeolaDS <deola.c.dsilva3@gmail.com>
2022-05-13 12:32:20 +0400

    updated files

--- basics.py
+++ basics.py
@@ -8,7 +8,13 @@ sentence = "Hello World"
 print(sentence.index("W"))
 print(sentence[0] + " - "+ sentence[6])
 print(sentence.replace("Hello", "Goodbye"))
-'''
+
 
 name = input("Enter your name: ")
 print("Hello, " + name)
+'''
+# ways to print 
+adj = 'sleepy'
+print("I'm {}".format(adj))
+print(f"I'm {adj}")
+print("I'm "+adj)
