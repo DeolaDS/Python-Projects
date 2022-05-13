c819391a73481bccb833c38ff6f55ceac9586b92
DeolaDS <deola.c.dsilva3@gmail.com>
2022-05-13 12:32:36 +0400

    Add madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,20 @@
+color = input("Color: ")
+creature = input("Creature: ")
+superlative = input("Superlative (ending with 'est'): ")
+adjective = input("Adjective: ")
+body_part =input("Body part: ")
+body_part2 =input("Plural body parts: ")
+noun= input("Noun: ")
+animal=input("Animal (plural): ")
+adjective1=input("Adjective: ")
+adjective2=input("Adjective: ")
+adjective3=input("Adjective: ")
+noun2= input("Noun: ")
+
+
+
+print(f"The {color} {creature} is the {superlative} of all. \
+It has {adjective} {body_part}, and a {body_part2} shaped like a {noun}. \
+It loves to eat {animal}, although it will feast on \
+nearly anything. It is {adjective1} and {adjective2}. You must be \
+{adjective3} around it, or you may end up as it`s {noun2}!")
